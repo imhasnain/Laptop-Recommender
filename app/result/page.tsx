e192bdeb3753642ab138ae3d6ab1bc31721bfa07
imhasnain <mirzaisback123@gmail.com>
2025-06-18 14:47:03 +0500

    Fix: convert /result page to client component

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,11 +1,6 @@
 'use client';
-import { Suspense } from 'react';
 import ResultComponent from './ResultComponent';
 
 export default function Page() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <ResultComponent />
-    </Suspense>
-  );
+  return <ResultComponent />;
 }
